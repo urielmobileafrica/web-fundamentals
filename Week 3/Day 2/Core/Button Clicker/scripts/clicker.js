bee7aa332668d2bbf4dc52eedcb7310bb6eb6bf7
Sam Keiru <sam@urielmobileafrica.com>
2023-12-16 00:35:15 +0000

    Re-submitted with verified solution with ifstatement tested and in comments

--- Week 3/Day 2/Core/Button Clicker/scripts/clicker.js
+++ Week 3/Day 2/Core/Button Clicker/scripts/clicker.js
@@ -7,7 +7,19 @@
 
 function changetxt(button) {
     console.log("Mouse Over!");
+    //original solution using onmouseover
     button.textContent = 'Logout'; // Changes the text of the button
+
+    //post review test updates after viewing solution
+    /*
+    
+    if(button.textContent == "Login"){
+        button.textContent="Logout";
+    }
+    else{
+        button.textContent="Login";
+    }
+    */
 
 };
 
