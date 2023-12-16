1a8730b8b77c0b7094409a8ea228cbdca47d788c
Sam Keiru <sam@urielmobileafrica.com>
2023-12-16 00:08:36 +0000

    Button Clicker Assignment

---
+++ Week 3/Day 2/Core/Button Clicker/scripts/clicker.js
@@ -0,0 +1,23 @@
+//changetxt()
+
+//function changetxt(button){
+    //console.log("button clicked!");
+  //  button.
+//};
+
+function changetxt(button) {
+    console.log("Mouse Over!");
+    button.textContent = 'Logout'; // Changes the text of the button
+
+};
+
+function changeback(button) {
+    console.log("Mouse Out!");
+    button.textContent = 'Login'; // Changes the text of the button
+
+};
+
+function disappear(button){
+    console.log("Vanish now!");
+    button.remove();
+};
