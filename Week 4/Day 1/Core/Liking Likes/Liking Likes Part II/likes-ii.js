fe94b94602213b4fcafde91b4de24483b77d892c
Sam Keiru <sam@urielmobileafrica.com>
2023-12-16 02:58:10 +0000

    Liking LIke Part I & Part II

---
+++ Week 4/Day 1/Core/Liking Likes/Liking Likes Part II/likes-ii.js
@@ -0,0 +1,30 @@
+//increment function
+
+
+
+function increment_1(){
+    var counter = document.querySelector("#counter_1");
+    var likes = parseInt(counter.textContent)
+    likes ++;
+    console.log(likes);
+    counter.textContent=likes;
+    return(likes);
+};
+
+function increment_2(){
+    var counter = document.querySelector("#counter_2");
+    var likes = parseInt(counter.textContent)
+    likes ++;
+    console.log(likes);
+    counter.textContent=likes;
+    return(likes);
+};
+
+function increment_3(){
+    var counter = document.querySelector("#counter_3");
+    var likes = parseInt(counter.textContent)
+    likes ++;
+    console.log(likes);
+    counter.textContent=likes;
+    return(likes);
+};
